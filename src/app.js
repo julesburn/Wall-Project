61c64c57b99872af44da9f9de6a8b9e9e49f6b80
julesburn <cmbenson88@gmail.com>
2019-08-19 14:43:35 -0500

    Completes Heroku setup

--- src/app.js
+++ src/app.js
@@ -1,7 +1,9 @@
+const appConfig = require("./config/main-config.js");
 const express = require("express");
 const app = express();
 const routeConfig = require("./config/route-config.js");
 
+appConfig.init();
 routeConfig.init(app);
 
 module.exports = app;
